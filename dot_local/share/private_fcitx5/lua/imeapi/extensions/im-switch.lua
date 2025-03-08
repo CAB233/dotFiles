e8803432f6e047daa0bf94e635ce68a562c298d1
CAB233 <yidaduizuoye@outlook.com>
2025-03-08 16:16:39 +0800

    Add .local/share/fcitx5/lua/imeapi/extensions/im-switch.lua

---
+++ dot_local/share/private_fcitx5/lua/imeapi/extensions/im-switch.lua
@@ -0,0 +1,19 @@
+-- 指定应用的初始化输入法
+-- 获取当前输入法名称：dbus-send --session --print-reply --dest=org.fcitx.Fcitx5 /controller org.fcitx.Fcitx.Controller1.CurrentInputMethod
+-- 获取 app_im: kde 中可以 “窗口管理” - “窗口规则” - “新增” - “检测窗口属性” - “窗口类(应用程序)”
+local fcitx = require("fcitx")
+
+local app_im = {
+  ['Alacritty'] = 'keyboard-us',
+  ['org.kde.dolphin'] = 'keyboard-us'
+  -- ['code'] = 'keyboard-us'
+}
+
+function onContextCreated()
+  local a = app_im[fcitx.currentProgram()]
+  if a then
+    fcitx.setCurrentInputMethod(a, true)
+  end
+end
+
+fcitx.watchEvent(fcitx.EventType.ContextCreated, "onContextCreated")
